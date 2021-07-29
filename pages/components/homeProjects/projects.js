61d38190fbc30077ff51fd594a15662b4f2c80b6
prateekbose20011 <smitraprateek2001@gmail.com>
2021-07-29 18:49:24 +0530

    Added Reveal Animation to Projects

--- pages/components/homeProjects/projects.js
+++ pages/components/homeProjects/projects.js
@@ -1,10 +1,27 @@
 import Image from 'next/image'
 import arrow from './arrow-right.svg'
+import { animated, useSpring } from 'react-spring'
+import * as easings from 'd3-ease'
 
 export default function Projects(){
+
+    const ProjectReveal = (index) => useSpring({
+        from: {
+            opacity: 0
+        },
+        to: {
+            opacity: 1
+        },
+        delay: 2000,
+        config: {
+            duration: 900,
+            easing: easings.easeCubic
+        }
+    })
+
     return (
         <section className="home-projects">
-            <div className="home-project">
+            <animated.div className="home-project" style={ProjectReveal(1)}>
                 <div className="project-image"></div>
                 <div className="project-nav">
                     <div className="project-details">
@@ -13,8 +30,8 @@ export default function Projects(){
                     </div>
                     <button className="project-explore"><Image src={arrow} alt=""/></button>
                 </div>
-            </div>
-            <div className="home-project">
+            </animated.div>
+            <animated.div className="home-project" style={ProjectReveal(0)}>
                 <div className="project-image"></div>
                 <div className="project-nav">
                     <div className="project-details">
@@ -23,8 +40,8 @@ export default function Projects(){
                     </div>
                     <button className="project-explore"><Image src={arrow} alt=""/></button>
                 </div>
-            </div>
-            <div className="home-project">
+            </animated.div>
+            <animated.div className="home-project" style={ProjectReveal(1)}>
                 <div className="project-image"></div>
                 <div className="project-nav">
                     <div className="project-details">
@@ -33,17 +50,8 @@ export default function Projects(){
                     </div>
                     <button className="project-explore"><Image src={arrow} alt=""/></button>
                 </div>
-            </div>
-            <div className="home-project">
-                <div className="project-image"></div>
-                <div className="project-nav">
-                    <div className="project-details">
-                        <h1>Project Name</h1>
-                        <h3>Project Details</h3>
-                    </div>
-                    <button className="project-explore"><Image src={arrow} alt=""/></button>
-                </div>
-            </div>
+            </animated.div>
+            
         </section>
     )
 }
