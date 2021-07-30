4a1d505ee464b9563c2042692b87c3297ae915a8
prateekbose20011 <smitraprateek2001@gmail.com>
2021-07-30 20:20:27 +0530

    resolved case discrepency

--- pages/components/footer/footer.js
+++ pages/components/footer/footer.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 export default function Footer(){
     return [
         <Head key={-1}>
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossOrigin="anonymous" referrerpolicy="no-referrer" />
         </Head>,
         <footer key={0}>
             <h1>Like my work,<br/>reach out on my socials</h1>
