7960f0c516f01fcb4bef977fc754ab166d031378
prateekbose20011 <smitraprateek2001@gmail.com>
2021-08-04 12:03:43 +0530

    Fixed order of sections in Footer

--- pages/components/footer/footer.js
+++ pages/components/footer/footer.js
@@ -3,17 +3,18 @@ export default function Footer(){
         <footer key={0}>
             <h1>Like my work,<br/>reach out on my socials</h1>
             <ul>
-                <li><h3>Resources</h3></li>
-                <li><a href="./projects">Projects</a></li>
-                <li><a href="./blogs">Blogs</a></li>
-                <li><a href="#">About</a></li>
-            </ul>
-            <ul>
                 <li><h3>Social</h3></li>
                 <li><a href="#">LinkedIn</a></li>
                 <li><a href="#">Github</a></li>
                 <li><a href="#">Email</a></li>
             </ul>
+            <ul>
+                <li><h3>Resources</h3></li>
+                <li><a href="./projects">Projects</a></li>
+                <li><a href="./blogs">Blogs</a></li>
+                <li><a href="#">About</a></li>
+            </ul>
+            
         </footer>
     )
 }
