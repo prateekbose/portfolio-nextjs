d6c99da73521f07d86efef61a7f3975a7fafe0e5
prateekbose20011 <smitraprateek2001@gmail.com>
2021-08-05 23:23:02 +0530

    Fixed Scaling Issue for Tablet and Mid Res Desktops

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import HeroSection from './components/heroSection/hero'
 import Projects from './components/homeProjects/projects'
 import Blogs from './components/homeBlogs/blogs'
 import Footer from './components/footer/footer'
+import Cards from './components/pageCards/pages'
 
 export default function Home() {
   return [
@@ -13,7 +14,7 @@ export default function Home() {
     </Head>,
     <NavBar key={0}/>,
     <HeroSection key={1} header={"I develop amazing experiences on Web and Mobile"} sub={"Hello"}/>,
-    <Projects key={2}/>,
+    <Cards key={2} page={"projects"}/>,
     // <Blogs key={3}/>,
     <Footer key={4}/>
   ]
