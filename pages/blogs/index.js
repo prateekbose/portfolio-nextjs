3061ae9e3c99816b0aaa6b0e742612bae2d9b70c
prateekbose <prateekbose20011@gmail.com>
2022-01-25 15:28:17 +0530

    Added Meta Tags for SEO
     - Added Meta Tags
    
     - Added Google Verification

--- pages/blogs/index.js
+++ pages/blogs/index.js
@@ -9,6 +9,16 @@ export default function Blogs() {
     <Head key={-1}>
       <title>Prateek Bose {' \u2014 '} Blogs</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name="description" content="full stack web developer" data-react-helmet="true"/>
+      <meta property="og:title" content="home" data-react-helmet="true"/>
+      <meta property="og:description" content="full stack web developer" data-react-helmet="true"/>
+      <meta property="og:type" content="website" data-react-helmet="true"/>
+      <meta name="twitter:card" content="summary" data-react-helmet="true"/>
+      <meta name="twitter:creator" content="Prateek Bose" data-react-helmet="true"/>
+      <meta name="twitter:title" content="home" data-react-helmet="true"/>
+      <meta name="twitter:description" content="full stack web developer" data-react-helmet="true"/>
+      <meta name="keywords" content="prateekbose, prateek, bose, portfolio, full-stack, javascript, react, nextjs" data-react-helmet="true"/>
+      <meta name="google-site-verification" content="63_J1iLK9zgMyFUpLRav8VgAIxuUzuMh6VCus7Xb8aU" />
     </Head>,
     <NavBar key={0}/>,
     <HeroSection key={1} header={"My two cents on things I find fascinating"} sub={"Blogs"}/>,
--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,16 @@ export default function Home() {
     <Head key={-1}>
       <title>Home &#8212; Prateek Bose</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name="description" content="full stack web developer" data-react-helmet="true"/>
+      <meta property="og:title" content="home" data-react-helmet="true"/>
+      <meta property="og:description" content="full stack web developer" data-react-helmet="true"/>
+      <meta property="og:type" content="website" data-react-helmet="true"/>
+      <meta name="twitter:card" content="summary" data-react-helmet="true"/>
+      <meta name="twitter:creator" content="Prateek Bose" data-react-helmet="true"/>
+      <meta name="twitter:title" content="home" data-react-helmet="true"/>
+      <meta name="twitter:description" content="full stack web developer" data-react-helmet="true"/>
+      <meta name="keywords" content="prateekbose, prateek, bose, portfolio, full-stack, javascript, react, nextjs" data-react-helmet="true"/>
+      <meta name="google-site-verification" content="63_J1iLK9zgMyFUpLRav8VgAIxuUzuMh6VCus7Xb8aU" />
     </Head>,
     <NavBar key={0}/>,
     <HeroSection key={1} header={"I develop amazing experiences on Web and Mobile"} sub={"Hello"}/>,
